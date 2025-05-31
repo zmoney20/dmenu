f0662311590a1250ad34660973c7bfc946be61ab
Zach <zachranderson@proton.me>
2025-05-31 10:37:35 -0600

    gruvbox selector color to match dwm

--- config.h
+++ config.h
@@ -1,22 +1,22 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const int user_bh = 10;               /* add an defined amount of pixels to the bar height */
+static const int user_bh =
+    10; /* add an defined amount of pixels to the bar height */
 
-static const char *fonts[] = {
-	"monospace:size=10"
-};
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *fonts[] = {"monospace:size=10"};
+static const char *prompt =
+    NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#ebdbb2", "#282828" },
-	[SchemeSel] = { "#ebdbb2", "#98971a" },
-	[SchemeOut] = { "#ebdbb2", "#8ec07c" },
+    /*     fg         bg       */
+    [SchemeNorm] = {"#ebdbb2", "#282828"},
+    [SchemeSel] = {"#ebdbb2", "#458588"},
+    [SchemeOut] = {"#ebdbb2", "#8ec07c"},
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines = 0;
 
 /*
  * Characters not considered part of a word while deleting words
